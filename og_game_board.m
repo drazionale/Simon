d6318351f2b202013413cb31e9738206fc460c37
drazionale <127063251+drazionale@users.noreply.github.com>
2023-03-11 12:13:12 -0500

    OG game board
    creates a basic image of the original Simon game board. Will be used as the base for the interactive image/game.

---
+++ og_game_board.m
@@ -0,0 +1,51 @@
+%url='https://haleyk.github.io/circle.png';
+%in_img = url;
+%filename ='circle.png';
+%outfilename = websave(filename,url);
+%%%%%%%%%%%% loads a circle image that acts as the base for the game board
+img=imread('circle.png');
+
+simon_board=zeros(250,250,3); %creates a 3d matrix of zeros
+
+for i=1:250                                     
+    for j= 1:250
+        if img(i,j,2)== 255                    %changes bkgrnd to black
+            img(i,j,:)=[0 0 0];    
+        end
+    end
+end
+simon_board=img;
+for i=1:250       %set quadrants of circle to correct colors     
+    for j= 1:250
+        if img(i,j,2)== 0 && img(i,j,1)== 255
+            if i <125 && j<125
+                simon_board(i,j,:)= [0 255 0];
+            elseif i >125 && j<125
+                simon_board(i,j,:)= [255 255 0];
+            elseif i>125 && j>125
+                    simon_board(i,j,:)= [0 0 255];
+            end
+        end
+    end
+end
+
+for i=50:100         %creates a black central shape (currently square)            
+    for j= 100:150
+        if img(i,j,1)== 255                    
+            simon_board(i+50,j,:)=[0 0 0];    
+        end
+    end
+end
+
+for i= 1:250 %for loop to create black crosshairs, separating colors
+    for j= 0:5
+simon_board(122+j,i,:)= [0 0 0];
+simon_board(i,122+j,:)= [0 0 0];
+    end
+end
+
+%%%prints "simon" in the center of the game board
+text="simon";
+position= [125 125];
+simon_board= insertText(simon_board,position,text,anchorpoint="Center",Boxcolor="black",textcolor="white");
+imagesc(simon_board)
